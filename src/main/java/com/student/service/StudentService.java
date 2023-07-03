d7ce21ba6d3cb1cf85926937545f6952445d5414
sivasuriyan <sivasuriyan0120@gmail.com>
2023-07-03 22:52:54 +0530

    switched to new branch

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.student.service;
 
-import java.util.List;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,26 +17,25 @@ public class StudentService {
 	StudentDao studentDao;
 
 	public String saveStudentDetails(Student student) throws AgeNotEligibleException, Exception {
+		
 		if (student.getAge() < 18) {
 			throw new AgeNotEligibleException("Age should not be below 18.!");
 		} else {
 			return studentDao.saveStudentDetails(student);
 		}
-
 	}
 
 	public String saveStudenstDetails(List<Student> student) throws AgeNotEligibleException, Exception {
-
-		for (Student stu : student) {
-			if (stu.getAge() < 18) {
-				throw new AgeNotEligibleException(stu.getId() + " age is  below 18.!");
-			} else {
-				continue;
-			}
-		}
-		return studentDao.saveStudentsDetails(student);
-
-	}
+		List<Student> savedObjects = student.stream().filter(x->x.getAge()>18).collect(Collectors.toList());
+		List<Student> lessthan18= student.stream().filter(x->x.getAge()<18).collect(Collectors.toList());
+				studentDao.saveStudentsDetails(savedObjects);
+				if(!lessthan18.isEmpty()) {
+					for(Student les :lessthan18) {
+						throw new AgeNotEligibleException(les.getName()+" data is not saved in database. age should not below 18");
+					}
+				}
+			 return "Data Saved Successfully";
+}
 
 	public List<Student> getStudentDetails() {
 		return studentDao.getStudentsDetails();
@@ -48,7 +48,7 @@ public class StudentService {
 	public String updateStudentById(Student student) {
 		return studentDao.updateStudentById(student);
 	}
-	
+
 	public List<Student> getNameByRollNo(int id) {
 		return studentDao.getName(id);
 	}
